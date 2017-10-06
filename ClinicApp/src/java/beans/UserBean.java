18e8c65e867213a17c5037aa7abd755606d8df69
jopi79 <jjolkowska@gmail.com>
2017-10-06 09:55:29 +0200

    Filtr + refactor

--- ClinicApp/src/java/beans/UserBean.java
+++ ClinicApp/src/java/beans/UserBean.java
@@ -1,3 +1,5 @@
+package beans;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +10,7 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import javax.faces.context.FacesContext;
+import model.UserRole;
 
 /**
  *
