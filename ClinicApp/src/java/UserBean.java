ab91a5972204e6cb59fa2d743e28d169b95db450
jopi79 <jjolkowska@gmail.com>
2017-10-06 09:16:50 +0200

    formularz logowania

---
+++ ClinicApp/src/java/UserBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Student
+ */
+@Named(value = "userBean")
+@SessionScoped
+public class UserBean implements Serializable {
+
+    private String login,password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    /**
+     * Creates a new instance of UserBean
+     */
+    public UserBean() {
+    }
+    
+}
